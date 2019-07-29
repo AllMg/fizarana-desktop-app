3d60d80d560d586cf3240c33588d033fc570cc5f
AllMg <ravosoaravosoa@gmail.com>
2019-07-29 21:50:03 +0300

    29072019 21:49

--- AppJava/src/program/Program.java
+++ AppJava/src/program/Program.java
@@ -5,11 +5,59 @@ import java.lang.reflect.Field;
 import java.net.ServerSocket;
 import java.nio.charset.Charset;
 
-public class Program {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Program extends Application {
 	
 	static ServerSocket serverSocket;
 
-	public static void main(String[] args) {
+	@Override
+	public void start(Stage stage) throws Exception {
+		GridPane gridPane = new GridPane();
+		gridPane.setMinSize(550, 400);
+		gridPane.setVgap(15); 
+	    gridPane.setHgap(15);  
+		gridPane.setAlignment(Pos.CENTER);
+		gridPane.setStyle("-fx-background-color: #242442;");
+		
+		Text textIP = new Text("IP");
+		Text textPort = new Text("Port");
+		textIP.setFill(Color.WHITE);
+		textIP.setStyle("-fx-font-size: 17px;-fx-font-weight: bold;");
+		textPort.setStyle("-fx-font: normal bold 20px 'serif'; -fx-text-fill: white;"); 
+		
+		TextField textFieldIP = new TextField();
+		TextField textFieldPort = new TextField();
+		
+		gridPane.add(textIP, 0, 0);
+	    gridPane.add(textPort, 0, 1);
+	    gridPane.add(textFieldIP, 1, 0);
+	    gridPane.add(textFieldPort, 1, 1);
+		
+		Scene scene = new Scene(gridPane);
+	       
+		stage.setTitle("Fizarana"); 
+		stage.setScene(scene);
+		stage.setResizable(false);
+		stage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		super.stop();
+		if(serverSocket != null && !serverSocket.isClosed()){
+			serverSocket.close();
+		}
+	}
+	
+	static void initTextEncoding(){
 		System.out.println("System file.encoding => "+System.getProperty("file.encoding"));
 		/*
 		 * On modifie l'encodage des caractères pour la lecture/ecriture de flux
@@ -29,10 +77,17 @@ public class Program {
 			e.printStackTrace();
 		}
 		System.out.println("System file.encoding => "+System.getProperty("file.encoding")+"\n");
+	}
+
+	public static void main(String[] args) {
+		initTextEncoding();
 		
 		long rankStart = -999999999;
 		try {
 			serverSocket = new ServerSocket(9191);
+			
+			Application.launch(args);
+			
 			while(true){
 				WebServer webServer = new WebServer(serverSocket.accept(), rankStart);
 				(new Thread(webServer)).start();
