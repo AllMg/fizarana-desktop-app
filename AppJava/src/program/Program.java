b1450841a68097fd505b58ecb1b3b127ff8a12de
AllMg <ravosoaravosoa@gmail.com>
2019-07-30 21:50:27 +0300

    30072019 21:50

--- AppJava/src/program/Program.java
+++ AppJava/src/program/Program.java
@@ -6,12 +6,17 @@ import java.net.ServerSocket;
 import java.nio.charset.Charset;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Program extends Application {
@@ -26,20 +31,48 @@ public class Program extends Application {
 	    gridPane.setHgap(15);  
 		gridPane.setAlignment(Pos.CENTER);
 		gridPane.setStyle("-fx-background-color: #242442;");
+		//gridPane.setGridLinesVisible(true);
+		
+		GridPane gridPaneField = new GridPane();
+		gridPaneField.setVgap(15); 
+		gridPaneField.setHgap(15);  
+		gridPaneField.setAlignment(Pos.CENTER);
+		//gridPaneField.setGridLinesVisible(true);
 		
 		Text textIP = new Text("IP");
 		Text textPort = new Text("Port");
-		textIP.setFill(Color.WHITE);
+		textIP.setFill(Color.valueOf("aliceblue"));
 		textIP.setStyle("-fx-font-size: 17px;-fx-font-weight: bold;");
-		textPort.setStyle("-fx-font: normal bold 20px 'serif'; -fx-text-fill: white;"); 
+		textPort.setFill(textIP.getFill());
+		textPort.setStyle(textIP.getStyle());
 		
 		TextField textFieldIP = new TextField();
 		TextField textFieldPort = new TextField();
 		
-		gridPane.add(textIP, 0, 0);
-	    gridPane.add(textPort, 0, 1);
-	    gridPane.add(textFieldIP, 1, 0);
-	    gridPane.add(textFieldPort, 1, 1);
+		Button button = new Button("Je valide");
+	    GridPane.setHalignment(button, HPos.RIGHT);
+	    
+	    StackPane stackPane = new StackPane();
+	    stackPane.setMinSize(300, 100);
+	    stackPane.setStyle("-fx-border-radius: 10px;-fx-border-color: #ff0000;-fx-font-size: 13px;");
+	    
+	    Label labelError = new Label("Impossible de lancer le partage");
+	    labelError.setTextFill(Color.valueOf("#ff0000"));
+	    Label labelSuccess = new Label("Tapez IP:Port dans la bare d'adresse\nd'un navigateur pour voir");
+	    labelSuccess.setTextFill(Color.valueOf("#33cc00"));
+	    labelSuccess.setTextAlignment(TextAlignment.CENTER);
+		
+	    gridPaneField.add(textIP, 0, 0);
+	    gridPaneField.add(textPort, 0, 1);
+	    gridPaneField.add(textFieldIP, 1, 0);
+	    gridPaneField.add(textFieldPort, 1, 1);
+	    gridPaneField.add(button, 0, 2, 2, 1);
+	    
+	    stackPane.getChildren().add(labelError);
+	    stackPane.getChildren().add(labelSuccess);
+	    
+	    gridPane.add(gridPaneField, 0, 0);
+	    gridPane.add(stackPane, 0, 1);
 		
 		Scene scene = new Scene(gridPane);
 	       
